ca5ddba2f2289f33be061899c7dbbc050b9796c3
Jeremiah Webb <illusion173@hotmail.com>
2024-10-15 20:04:20 -0400

    FinishedSHA

--- AWS/iac-microbenchmark/lambdas/c#/sha256/src/sha256/Function.cs
+++ AWS/iac-microbenchmark/lambdas/c#/sha256/src/sha256/Function.cs
@@ -1,111 +1,121 @@
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
 using System.Text.Json;
-using System.Collections.Generic;
-using System;
-using System.Security.Cryptography;
 using Amazon.KeyManagementService;
 using Amazon.KeyManagementService.Model;
 using Amazon;
+using System.Text;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into .NET types
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace LambdaApiProxy
 {
-  // Struct representing the incoming request from body
-  public struct sha256Request
-  {
-    public string Message { get; set; }
-  }
-
-  // Struct representing the outgoing response into body
-  public struct sha256Response
-  {
-    public string ResponseMessage { get; set; }
-  }
-
-  public class Function
-  {
-    // The Lambda handler function
-    public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+    // Struct representing the incoming request from body
+    public struct sha256Request
     {
-      // Build APIGW response
-      var APIGWResponse = new APIGatewayProxyResponse
-      {
-        Headers = new Dictionary<string, string>
+        public string Message { get; set; }
+    }
+
+    // Struct representing the outgoing response into body
+    public struct sha256Response
+    {
+        public string signature { get; set; }
+    }
+
+    public class Function
+    {
+        private static readonly string SIGN_ALGORITHM = "HMAC_SHA_256";
+
+        // Create the KMS client
+        private static readonly IAmazonKeyManagementService kmsClient = new AmazonKeyManagementServiceClient(RegionEndpoint.USEast1);
+        // The Lambda handler function
+        public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+        {
+            // Build APIGW response
+            var APIGWResponse = new APIGatewayProxyResponse
+            {
+                Headers = new Dictionary<string, string>
             {
                 { "Content-Type", "application/json" },
                 {"Access-Control-Allow-Origin", "*"}
             }
-      };
+            };
 
+            string KMSKEYARNNAME = "SHA256_KMS_KEY_ARN";
 
-      string KMSKEYARNNAME = "_KMS_KEY_ARN";
+            // Get the value of the environment variable
+            string? KMSKEYARNVALUE = Environment.GetEnvironmentVariable(KMSKEYARNNAME);
 
-      // Get the value of the environment variable
-      string KMSKEYARNVALUE = Environment.GetEnvironmentVariable(KMSKEYARNNAME);
+            // Check if the variable is found
+            if (string.IsNullOrEmpty(KMSKEYARNVALUE))
+            {
+                Console.WriteLine($"Environment variable '{KMSKEYARNNAME}' is not set.");
 
-      // Check if the variable is found
-      if (string.IsNullOrEmpty(KMSKEYARNVALUE))
-      {
-        Console.WriteLine($"Environment variable '{KMSKEYARNNAME}' is not set.");
-
-        // Log the exception and return a 500 error response
-        var errorRsp = new Dictionary<string, string>
+                // Log the exception and return a 500 error response
+                var errorRsp = new Dictionary<string, string>
             {
                 { "Error", $"Environment variable '{KMSKEYARNNAME}' is not set." }
             };
 
-        APIGWResponse.StatusCode = 500;
-        APIGWResponse.Body = JsonSerializer.Serialize(errorRsp);
+                APIGWResponse.StatusCode = 500;
+                APIGWResponse.Body = JsonSerializer.Serialize(errorRsp);
 
-        return APIGWResponse;
-      }
+                return APIGWResponse;
+            }
 
+            try
+            {
+                // Deserialize the incoming request body into the RequestModel struct
+                var requestModel = JsonSerializer.Deserialize<sha256Request>(request.Body);
 
-
-
-      try
-      {
-        // Deserialize the incoming request body into the RequestModel struct
-        var requestModel = JsonSerializer.Deserialize<sha256Request>(request.Body);
-
-        // Create a response model with a message
-        var responseModel = new sha256Response
-        {
-          ResponseMessage = $"Hello, {requestModel.Message}!",
-        };
+                var message = requestModel.Message;
 
 
-        var region = RegionEndpoint.USEast1;
+                // Convert the message to bytes
+                byte[] messageBytes = Encoding.UTF8.GetBytes(message);
 
-        // Create the KMS client
-        using var kmsClient = new AmazonKeyManagementServiceClient(region);
+                // Call AWS KMS to generate MAC
+                var genMacResponse = await kmsClient.GenerateMacAsync(new GenerateMacRequest
+                {
+                    KeyId = KMSKEYARNVALUE,
+                    Message = new MemoryStream(messageBytes),
+                    MacAlgorithm = SIGN_ALGORITHM
+                });
 
-        // Serialize the response model into a JSON string
-        var responseBody = JsonSerializer.Serialize(responseModel);
+                // Base64 encode the MAC
+                string signatureb64 = Convert.ToBase64String(genMacResponse.Mac.ToArray());
 
-        // Return a successful response with the serialized response body
-        APIGWResponse.Body = responseBody;
-        APIGWResponse.StatusCode = 200;
 
-        return APIGWResponse;
-      }
-      catch (Exception ex)
-      {
-        // Log the exception and return a 500 error response
-        context.Logger.LogError($"Error processing request for sha256 c#: {ex.Message}");
-        var errorRsp = new Dictionary<string, string>
+                // Create a response model with a message
+                var responseModel = new sha256Response
+                {
+                    signature = signatureb64,
+                };
+
+                // Serialize the response model into a JSON string
+                var responseBody = JsonSerializer.Serialize(responseModel);
+
+                // Return a successful response with the serialized response body
+                APIGWResponse.Body = responseBody;
+                APIGWResponse.StatusCode = 200;
+
+                return APIGWResponse;
+            }
+            catch (Exception ex)
+            {
+                // Log the exception and return a 500 error response
+                context.Logger.LogError($"Error processing request for sha256 c#: {ex.Message}");
+                var errorRsp = new Dictionary<string, string>
             {
                 { "Error", $"Error processing request for sha256 c#: {ex.Message}" }
             };
 
-        APIGWResponse.StatusCode = 500;
-        APIGWResponse.Body = JsonSerializer.Serialize(errorRsp);
+                APIGWResponse.StatusCode = 500;
+                APIGWResponse.Body = JsonSerializer.Serialize(errorRsp);
 
-        return APIGWResponse;
-      }
+                return APIGWResponse;
+            }
+        }
     }
-  }
 }
--- AWS/iac-microbenchmark/lambdas/c#/sha384/src/sha384/Function.cs
+++ AWS/iac-microbenchmark/lambdas/c#/sha384/src/sha384/Function.cs
@@ -1,111 +1,129 @@
 using Amazon.Lambda.Core;
 using Amazon.Lambda.APIGatewayEvents;
 using System.Text.Json;
-using System.Collections.Generic;
-using System;
-using System.Security.Cryptography;
 using Amazon.KeyManagementService;
 using Amazon.KeyManagementService.Model;
 using Amazon;
+using System.Text;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into .NET types
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
 
 namespace LambdaApiProxy
 {
-// Struct representing the incoming request from body
-public struct sha384Request
-{
+  // Struct representing the incoming request from body
+  public struct sha384Request
+  {
     public string Message { get; set; }
-}
+  }
 
-// Struct representing the outgoing response into body
-public struct sha384Response
-{
-    public string ResponseMessage { get; set; }
-}
+  // Struct representing the outgoing response into body
+  public struct sha384Response
+  {
+    public string signature { get; set; }
+  }
 
-public class Function
-{
+  public class Function
+  {
+    private static readonly string SIGN_ALGORITHM = "HMAC_SHA_384";
+
+    // Create the KMS client
+    private static readonly IAmazonKeyManagementService kmsClient = new AmazonKeyManagementServiceClient(RegionEndpoint.USEast1);
     // The Lambda handler function
-    public APIGatewayProxyResponse FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
+    public async Task<APIGatewayProxyResponse> FunctionHandler(APIGatewayProxyRequest request, ILambdaContext context)
     {
       // Build APIGW response
-        var APIGWResponse = new APIGatewayProxyResponse
-        {
-            Headers = new Dictionary<string, string>
+      var APIGWResponse = new APIGatewayProxyResponse
+      {
+        Headers = new Dictionary<string, string>
             {
                 { "Content-Type", "application/json" },
                 {"Access-Control-Allow-Origin", "*"}
             }
-        };
+      };
 
+      string KMSKEYARNNAME = "SHA384_KMS_KEY_ARN";
 
-        string KMSKEYARNNAME = "_KMS_KEY_ARN";
+      // Get the value of the environment variable
+      string? KMSKEYARNVALUE = Environment.GetEnvironmentVariable(KMSKEYARNNAME);
 
-        // Get the value of the environment variable
-        string KMSKEYARNVALUE = Environment.GetEnvironmentVariable(KMSKEYARNNAME);
+      // Check if the variable is found
+      if (string.IsNullOrEmpty(KMSKEYARNVALUE))
+      {
+        Console.WriteLine($"Environment variable '{KMSKEYARNNAME}' is not set.");
 
-        // Check if the variable is found
-        if (string.IsNullOrEmpty(KMSKEYARNVALUE))
-        {
-            Console.WriteLine($"Environment variable '{KMSKEYARNNAME}' is not set.");
-
-            // Log the exception and return a 500 error response
-            var errorRsp = new Dictionary<string, string>
+        // Log the exception and return a 500 error response
+        var errorRsp = new Dictionary<string, string>
             {
                 { "Error", $"Environment variable '{KMSKEYARNNAME}' is not set." }
             };
 
-            APIGWResponse.StatusCode = 500;
-            APIGWResponse.Body = JsonSerializer.Serialize(errorRsp);
+        APIGWResponse.StatusCode = 500;
+        APIGWResponse.Body = JsonSerializer.Serialize(errorRsp);
 
-            return APIGWResponse;
-        }
+        return APIGWResponse;
+      }
 
 
 
-        
-        try
+
+
+
+
+
+
+      try
+      {
+        // Deserialize the incoming request body into the RequestModel struct
+        var requestModel = JsonSerializer.Deserialize<sha384Request>(request.Body);
+
+        var message = requestModel.Message;
+
+
+        // Convert the message to bytes
+        byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+        // Call AWS KMS to generate MAC
+        var genMacResponse = await kmsClient.GenerateMacAsync(new GenerateMacRequest
         {
-            // Deserialize the incoming request body into the RequestModel struct
-            var requestModel = JsonSerializer.Deserialize<sha384Request>(request.Body);
+          KeyId = KMSKEYARNVALUE,
+          Message = new MemoryStream(messageBytes),
+          MacAlgorithm = SIGN_ALGORITHM
+        });
 
-            // Create a response model with a message
-            var responseModel = new sha384Response
-            {
-                ResponseMessage = $"Hello, {requestModel.Message}!",
-            };
+        // Base64 encode the MAC
+        string signatureb64 = Convert.ToBase64String(genMacResponse.Mac.ToArray());
 
 
-            var region = RegionEndpoint.USEast1;
-
-            // Create the KMS client
-            using var kmsClient = new AmazonKeyManagementServiceClient(region);
-
-            // Serialize the response model into a JSON string
-            var responseBody = JsonSerializer.Serialize(responseModel);
-
-            // Return a successful response with the serialized response body
-            APIGWResponse.Body = responseBody;
-            APIGWResponse.StatusCode = 200;
-
-            return APIGWResponse;
-        }
-        catch (Exception ex)
+        // Create a response model with a message
+        var responseModel = new sha384Response
         {
-            // Log the exception and return a 500 error response
-            context.Logger.LogError($"Error processing request for sha384 c#: {ex.Message}");
-            var errorRsp = new Dictionary<string, string>
+          signature = signatureb64,
+        };
+
+        // Serialize the response model into a JSON string
+        var responseBody = JsonSerializer.Serialize(responseModel);
+
+        // Return a successful response with the serialized response body
+        APIGWResponse.Body = responseBody;
+        APIGWResponse.StatusCode = 200;
+
+        return APIGWResponse;
+      }
+      catch (Exception ex)
+      {
+        // Log the exception and return a 500 error response
+        context.Logger.LogError($"Error processing request for sha384 c#: {ex.Message}");
+        var errorRsp = new Dictionary<string, string>
             {
                 { "Error", $"Error processing request for sha384 c#: {ex.Message}" }
             };
 
-            APIGWResponse.StatusCode = 500;
-            APIGWResponse.Body = JsonSerializer.Serialize(errorRsp);
+        APIGWResponse.StatusCode = 500;
+        APIGWResponse.Body = JsonSerializer.Serialize(errorRsp);
 
-            return APIGWResponse;
-        }
+        return APIGWResponse;
+      }
     }
-}
+  }
 }
