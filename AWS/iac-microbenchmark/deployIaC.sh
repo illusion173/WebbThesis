901c3ab48e3f4320a04d46aa2475fda78ee4dc49
Jeremiah Webb <illusion173@hotmail.com>
2025-01-08 10:50:32 -0500

    Need to Have custom script for deployment due to acct quotas

---
+++ AWS/iac-microbenchmark/deployIaC.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# For some reason timeouts are an issue while deploying, this will automate per stack, similar to --all
+#
+# List of stack names
+stacks=(
+    #"IaCBenchMark-Build-Lambdas-csharp-x86"
+    #"IaCBenchMark-Build-Lambdas-go-x86"
+    #"IaCBenchMark-Build-Lambdas-java-x86"
+    #"IaCBenchMark-Build-Lambdas-python-x86"
+    "IaCBenchMark-Build-Lambdas-rust-x86"
+    "IaCBenchMark-Build-Lambdas-typescript-x86"
+    "IaCBenchMark-Build-Lambdas-csharp-arm"
+    "IaCBenchMark-Build-Lambdas-go-arm"
+    "IaCBenchMark-Build-Lambdas-java-arm"
+    "IaCBenchMark-Build-Lambdas-python-arm"
+    "IaCBenchMark-Build-Lambdas-rust-arm"
+    "IaCBenchMark-Build-Lambdas-typescript-arm"
+)
+
+# Loop through each stack
+for stack in "${stacks[@]}"
+do
+    echo "Deploying $stack..."
+    cdk deploy --require-approval never "$stack"
+
+    # Check if the command succeeded
+    if [ $? -ne 0 ]; then
+        echo "Deployment failed for $stack. Exiting."
+        exit 1
+    fi
+
+    # Sleep for 1 minute
+    echo "Sleeping for 1 minute before deploying the next stack..."
+    sleep 60
+done
+
+echo "All stacks deployed successfully!"
